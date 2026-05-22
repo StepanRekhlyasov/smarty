93062aa8b7e6ebe9e1112c2ee6b27e6cd11a62a5
Степан Рехлясов <srekhlyasov@gmail.com>
2026-05-22 13:12:49 +0400

    scss cache

--- www/assets/style.php
+++ www/assets/style.php
@@ -5,15 +5,31 @@ require __DIR__ . '/../vendor/autoload.php';
 use ScssPhp\ScssPhp\Compiler;
 use ScssPhp\ScssPhp\OutputStyle;
 
-$scssDir  = __DIR__ . '/scss';
-$mainFile = $scssDir . '/main.scss';
+$scssDir   = __DIR__ . '/scss';
+$mainFile  = $scssDir . '/main.scss';
+$cacheFile = sys_get_temp_dir() . '/smarty_style_' . md5($scssDir) . '.css';
 
-$compiler = new Compiler();
-$compiler->setImportPaths($scssDir);
-$compiler->setOutputStyle(OutputStyle::COMPRESSED);
+// Находим самый свежий mtime среди всех .scss-файлов
+$scssFiles   = glob($scssDir . '/*.scss') ?: [];
+$latestMtime = 0;
+foreach ($scssFiles as $f) {
+    $latestMtime = max($latestMtime, filemtime($f));
+}
 
-$css = $compiler->compileString(file_get_contents($mainFile))->getCss();
+$cacheValid = file_exists($cacheFile) && filemtime($cacheFile) >= $latestMtime;
+
+if (!$cacheValid) {
+    $compiler = new Compiler();
+    $compiler->setImportPaths($scssDir);
+    $compiler->setOutputStyle(OutputStyle::COMPRESSED);
+
+    $css = $compiler->compileString(file_get_contents($mainFile))->getCss();
+    file_put_contents($cacheFile, $css, LOCK_EX);
+} else {
+    $css = file_get_contents($cacheFile);
+}
 
 header('Content-Type: text/css; charset=utf-8');
-header('Cache-Control: no-cache');
+header('Cache-Control: public, max-age=3600');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $latestMtime) . ' GMT');
 echo $css;
